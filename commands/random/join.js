4ab5d642d009373b90d8dca4dee067b36f6c6a78
Dielz <darkfalz674@gmail.com>
2019-06-30 20:11:50 -0400

    02

---
+++ commands/random/join.js
@@ -0,0 +1,34 @@
+const commando = require('discord.js-commando');
+
+class joinCommand extends commando.Command {
+
+constructor(client) {
+    super(client, {
+        name:'join',
+        group: 'random',
+        memberName: 'join',
+        description: 'cody entra a mi canal' 
+    });
+}
+
+async run(message, args){
+
+    if(message.member.voiceChannel)
+    {
+      if(message.guild.voiceConnection)
+      {
+        message.member.voiceChannel.join()
+        .then(connection => {
+            message.replay("Aqui estoy!");
+        })
+
+      }
+    }
+
+} 
+
+}
+
+
+
+module.exports = joinCommand;
